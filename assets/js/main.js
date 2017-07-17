fd2249e89a22ae98779d54f6d4f93b0e75d1f0b9
Daren Puan <darenpuan@gmail.com>
2017-07-17 15:16:38 +0800

    Daren
    Imported images from nicole

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.nav li').click(function () {
+    $('.navbar-nav li').click(function () {
         $(this).addClass('active');
         $(this).siblings().removeClass('active');
     });
