f92191245b834474569b9687d639723c07e1e9e1
Daren Puan <darenpuan@gmail.com>
2017-07-17 18:57:30 +0800

    Daren
    contact section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,26 @@ $(document).ready(function () {
         $(this).siblings().removeClass('active');
     });
 });
+
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var subject = document.forms["contactForm"]["subject"].value;
+    var msg = document.forms["contactForm"]["message"].value;
+    if (name == "") {
+        alert("Please fill in your name.");
+        return false;
+    }
+    if (email == "") {
+        alert("Please fill in your email address.");
+        return false;
+    }
+    if (subject == "") {
+        alert("Please fill in a subject.");
+        return false;
+    }
+    if (msg == "") {
+        alert("Please leave a short message for us.");
+        return false;
+    }
+}
